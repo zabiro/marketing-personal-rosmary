143ebd6d7bd86dd659e94ed205322f86eb57d983
luciano Deluque <lucianoadb@gmail.com>
2018-12-21 12:21:05 -0500

    commit configurada mysql en ruby on rails

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  get 'home/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+end
